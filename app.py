0247b705f7170c45bdd4144dff89808804cf23ba
Anjo Cordero <anjocordero@gmail.com>
2024-04-24 18:25:02 -0700

    fixed filtering for graph

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-"""
+_ = """
 Import libraries
 """
 import pandas as pd
 import streamlit as st
 import plotly_express as px
 
-"""
+_ = """
 Read in and enrich DataFrame, as explained in EDA.ipynb
 """
 
@@ -13,25 +13,27 @@ df = pd.read_csv("vehicles_us.csv")
 df['is_4wd'] = df['is_4wd'] == 1.0
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
-"""
+_ = """
 Display graphs
 """
 
 st.header("An Analysis of Cars on Sale in the US")
 
 manualSelected = st.checkbox("Manual transmission?")
+numBins = 700
 
 df_filtered = df.copy()
 
 if manualSelected:
-    df_filtered = df_filtered['transmission'] == 'manual'
+    df_filtered = df_filtered[ df_filtered['transmission'] == 'manual' ]
+    numBins = 200
 else:
-    df_filtered = df_filtered['transmission'] == 'automatic'
+    df_filtered = df_filtered[ df_filtered['transmission'] == 'automatic' ]
 
 fig = px.histogram(
     df_filtered['price'],
     range_x=[0, 75000],
-    nbins=700
+    nbins=numBins
     )
 
 st.write(fig)
