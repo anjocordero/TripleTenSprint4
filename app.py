7361434bc2246161d65f6ff7fd14e2f88bdc712c
Anjo Cordero <anjocordero@gmail.com>
2024-04-24 18:08:16 -0700

    initial figures drawn

--- app.py
+++ app.py
@@ -1,5 +1,37 @@
+"""
+Import libraries
+"""
+import pandas as pd
 import streamlit as st
+import plotly_express as px
 
-st.header('Tossing a Coin')
+"""
+Read in and enrich DataFrame, as explained in EDA.ipynb
+"""
 
-st.write('It is not a functional application yet. Under construction.')
+df = pd.read_csv("vehicles_us.csv")
+df['is_4wd'] = df['is_4wd'] == 1.0
+df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
+
+"""
+Display graphs
+"""
+
+st.header("An Analysis of Cars on Sale in the US")
+
+manualSelected = st.checkbox("Manual transmission?")
+
+df_filtered = df.copy()
+
+if manualSelected:
+    df_filtered = df_filtered['transmission'] == 'manual'
+else:
+    df_filtered = df_filtered['transmission'] == 'automatic'
+
+fig = px.histogram(
+    df_filtered['price'],
+    range_x=[0, 75000],
+    nbins=700
+    )
+
+st.write(fig)
